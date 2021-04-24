a3b14d879a48e43f02a3fb2b3f5b516132673516
chap95 <chap95@likelion.org>
2021-04-24 21:41:16 +0900

    add: playFor function

--- amountFor.ts
+++ amountFor.ts
@@ -1,23 +1,60 @@
-import { IPerformace, IPlays, PlayType } from "./raw_code";
+import { IInvoice, IPerformace, IPlays, PlayType } from "./rawCode";
 
-export default function amountFor(aPerformance: IPerformace, play: IPlays) {
-	let result = 0;
-	switch (play.type) {
-		case PlayType.TRAGEDY: //비극
-			result = 40000;
-			if (aPerformance.audiance > 30) {
-				result += 1000 * (aPerformance.audiance - 30);
+
+
+
+
+function statement(invoice: IInvoice, plays: IPlays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명 ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+	
+// 1. 함수 추출하기 switch case문으로 입장료를 계산하던 로직을 함수로 만들기
+	function amountFor(aPerformance: IPerformace, play: IPlays) {
+		let result = 0;
+		switch (play.type) {
+			case PlayType.TRAGEDY: //비극
+				result = 40000;
+				if (aPerformance.audiance > 30) {
+					result += 1000 * (aPerformance.audiance - 30);
+				}
+				break;
+			case PlayType.COMEDY: //희극
+				result = 30000;
+				if (aPerformance.audiance > 20) {
+					result += 10000 + 500 * (aPerformance.audiance - 20);
+				}
+				result += 300 * aPerformance.audiance;
+				break;
+			default:
+				throw new Error(`알 수 없는 장르 ${play}`);
 			}
-			break;
-		case PlayType.COMEDY: //희극
-			result = 30000;
-			if (aPerformance.audiance > 20) {
-				result += 10000 + 500 * (aPerformance.audiance - 20);
-			}
-			result += 300 * aPerformance.audiance;
-			break;
-		default:
-			throw new Error(`알 수 없는 장르 ${play}`);
-		}
-	return result
+		return result
+	}
+
+  for (let perf of invoice.preformances) {
+    const play: IPlays = plays[perf.playID];
+    let thisAmount = amountFor(perf, play); // 아래에 있던 switch case를 amountFor 함수로 추출
+
+    volumeCredits += Math.max(perf.audiance - 30, 0); //포인트 적립
+    if (play.type === PlayType.COMEDY) {
+      volumeCredits += Math.floor(perf.audiance / 5);
+    }
+
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audiance
+    }석)\n`;
+
+    totalAmount += thisAmount;
+  }
+
+  result += `총액: ${format(totalAmount)}\n`;
+  result += `적립 포인트: ${volumeCredits}점 \n`;
+
+  return result;
 }
---
+++ playFor.ts
@@ -0,0 +1,67 @@
+import { IInvoice, IPerformace, IPlays, PlayType } from './rawCode';
+
+// 2. 임시 변수를 질의 함수로 바꾸기 
+// amountFor 안에서 play는 aPerformance를 통해 얻을 수 있기 때문에 임시 변수를 제거한다.
+// 지역적 범위에 존재하는 이름이 늘어나서 추출작업이 어렵기 때문이다.
+// amountFor를 호출하는 반복문 안에서 보면 play는 perf에 따라 변한다. amountFor에 play 값과 perf 값을
+// 따로 전달하는 건 이상해 보인다. amountFor안에서 play 값을 결정해주면 좋아보인다.
+
+function statement(invoice: IInvoice, plays: IPlays) {
+
+  function amountFor(aPerformance: IPerformace) { // play 파라미터가 있었지만 play를 내부에서 정해주기 때문에 파라미터에서 제거해 줬다.
+    let result = 0;
+		const play = playFor(aPerformance); // aPerformace에 따라 변하는 값 함수로 추출
+    switch (play.type) {
+      case PlayType.TRAGEDY: //비극
+        result = 40000;
+        if (aPerformance.audiance > 30) {
+          result += 1000 * (aPerformance.audiance - 30);
+        }
+        break;
+      case PlayType.COMEDY: //희극
+        result = 30000;
+        if (aPerformance.audiance > 20) {
+          result += 10000 + 500 * (aPerformance.audiance - 20);
+        }
+        result += 300 * aPerformance.audiance;
+      break;
+      default:
+        throw new Error(`알 수 없는 장르 ${play}`);
+    }
+    return result;
+  }
+
+  function playFor(aPerformance: IPerformace) {
+    return plays[aPerformance.playID];
+  }
+
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명 ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.preformances) {
+   // const play: IPlays = plays[perf.playID];  play 라는 변수를 playFor 함수로 인라인 시켰다. 임시 변수 사용을 최소화 하기 위해
+    let thisAmount = amountFor(perf); // play 인자를 제거 함수 내부에서 play를 결정
+
+    volumeCredits += Math.max(perf.audiance - 30, 0);
+    if (playFor(perf).type === PlayType.COMEDY) {
+      volumeCredits += Math.floor(perf.audiance / 5);
+    }
+
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
+      perf.audiance
+    }석)\n`;
+
+    totalAmount += thisAmount;
+  }
+
+  result += `총액: ${format(totalAmount)}\n`;
+  result += `적립 포인트: ${volumeCredits}점 \n`;
+
+  return result;
+}
